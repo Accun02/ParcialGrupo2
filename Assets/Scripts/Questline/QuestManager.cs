b23cf52cdb0c31404dfced29a8ae59bc608d7f80
Accun02 <fhermopelufo@uade.edu.ar>
2024-09-30 12:41:26 -0300

    Questline

--- Assets/Scripts/Questline/QuestManager.cs
+++ Assets/Scripts/Questline/QuestManager.cs
@@ -8,38 +8,47 @@ using static UnityEditor.Progress;
 public class QuestManager : MonoBehaviour
 {
     private Queue <GameObject> missionQueue = new Queue <GameObject> ();
-    [SerializeField] private GameObject[] quests;
-
-    [SerializeField] private Transform questpos;
+    [SerializeField] private GameObject[] quests; // array que guarda las primeras instancias de las quest
+    private GameObject currentMission;
+    [SerializeField] private Transform questpos; //que se quede centrada en pantalla
     // Start is called before the first frame update
     void Start()
     {
         
-        for (int i = 0; i < quests.Length; i++) 
+        for (int i = 0; i < quests.Length; i++)  //Guarda las quest en la cola
         {
-            missionQueue.Enqueue(quests[i]);
-       
+            missionQueue.Enqueue(quests[i]); 
+         
         }
-      
-        
-        missionQueue.TryPeek(out GameObject newquest);
-        Instantiate( newquest, questpos );
-
-        
+        missionQueue.TryPeek(out GameObject newquest); //se fija;
+        currentMission = Instantiate( newquest, questpos );   //se intancia y se guarda en el gameobject current mission
     }
 
     // Update is called once per frame
      public void FinishQuest()
     {
-        if (missionQueue.TryDequeue(out GameObject finishdmission))
+
+
+        if (missionQueue.Count == 0)
         {
-            Destroy(finishdmission);
+            Debug.Log("Vuelve en 24 horas");
         }
-     
+        else
+        {
+            missionQueue.TryDequeue(out GameObject finishMission); //se fija si hay un objeto
 
-        missionQueue.TryPeek(out GameObject newquest);
-        Instantiate( newquest, questpos );
+            if (finishMission != null)
+            {
+                Destroy(currentMission); // destruye la mision actual
+            }
 
-     
+
+
+            missionQueue.TryPeek(out GameObject newquest); //se fija si hay nuevas
+            currentMission = Instantiate(newquest, questpos); // la intancia
+        }
+        
+
     }
+
 }
